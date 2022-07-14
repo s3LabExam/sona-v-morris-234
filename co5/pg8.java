a134841918d09b2715c8f830908b31927f7887b4
sonavmorris <93874702+sonavmorris@users.noreply.github.com>
2022-07-14 20:47:27 +0530

    Create pg8.java

---
+++ co5/pg8.java
@@ -0,0 +1,24 @@
+import java.awt.*; 
+import java.awt.event.*; 
+import java.applet.*;
+public class q8 extends Applet implements KeyListener{
+	String msg=" "; 
+	int x=30,y=50; 
+	public void init(){
+		addKeyListener(this); 
+		requestFocus();
+	}
+	public void keyTyped(KeyEvent ke){
+		msg+=ke.getKeyChar();
+		repaint();
+	}
+	public void keyReleased(KeyEvent ke){
+		showStatus("Key Up!");
+	}
+	public void keyPressed(KeyEvent ke){
+		showStatus("Key Down!");
+	}
+	public void paint(Graphics G){
+		G.drawString(msg,x,y);
+	}
+}
