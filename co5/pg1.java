0735e25bad187b38c05225f1380d1c6ae9ce89fc
sonavmorris <93874702+sonavmorris@users.noreply.github.com>
2022-07-14 20:43:46 +0530

    Create pg1.java

---
+++ co5/pg1.java
@@ -0,0 +1,12 @@
+import java.applet.*;
+import java.awt.*;
+
+public class q1 extends Applet{
+	public void paint(Graphics g){
+		g.drawLine(20,20,50,20);
+		
+		g.drawOval(200,200,100,100);
+		
+		g.drawRect(100,100,100,100);
+	}
+}
